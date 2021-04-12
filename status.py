f8d48d1b5e046ad8ecb70b37e832a3f88834cd11
qbxt <10813JJ@gmail.com>
2021-04-12 12:46:27 -0600

    update event listeners

--- status.py
+++ status.py
@@ -1,3 +1,5 @@
+import datetime
+
 import discord
 from discord.ext import commands
 
@@ -15,9 +17,12 @@ class Status(commands.Cog):
 
     @commands.Cog.listener("on_guild_join")
     async def on_guild_join(self, guild):
-        e = discord.Embed(title="exec log", description="joined guild", color=discord.Color(9510889))
+        e = discord.Embed(title="exec log", description="joined guild", color=discord.Color(9510889),
+                          timestamp=datetime.datetime.now())
         e.add_field(name="name", value=guild.name.lower())
         e.add_field(name="members", value=str(guild.member_count))
+        e.set_footer(text="exec", icon_url="https://cdn.discordapp.com/avatars/830972631917789265"
+                                           "/5e97d058954d564c39b6e1d91ad09e39.png")
         await self.logger.log_embed(e)
 
     # TODO add event queue for log messages (similar to Neptune)
