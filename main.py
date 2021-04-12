d2015fb0ab4a12bb1bc26f3b3ebae8c568f99050
qbxt <10813JJ@gmail.com>
2021-04-12 12:31:30 -0600

    update prefix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 
 def main():
-    bot = commands.Bot(command_prefix="exec")
+    bot = commands.Bot(command_prefix=commands.when_mentioned_or("exec"))
     bot.remove_command("help")
 
     env = os.getenv("EXECBOT_ENV_TYPE")
