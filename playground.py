424b9a2c58655c84b120037061022185ab3e3bb9
qbxt <10813JJ@gmail.com>
2021-04-12 13:03:18 -0600

    reenable networking

--- playground.py
+++ playground.py
@@ -117,7 +117,7 @@ class Playground(commands.Cog):
                                                        cpu_shares=512,
                                                        mem_limit="512m",
                                                        device_write_bps=[{"Path": "/dev/sda", "Rate": 500000}],
-                                                       network_disabled=True)
+                                                       network_disabled=False)
 
             t = threading.Thread(target=get_logs_from_container,
                                  name=str(ctx.message.id),
