41f2b8b097e700ea13abcd6ea0fd82e997626d10
qbxt <10813JJ@gmail.com>
2021-10-04 14:23:34 -0500

    remove typing indicator

--- discord/playground.py
+++ discord/playground.py
@@ -67,112 +67,109 @@ class Playground(commands.Cog):
     async def _exec(self, ctx):
         """Execute a code snippet and post the result."""
 
-        async with ctx.typing():
-            code = ""
-            lang = ""
-            # TODO read from JSON
-            regexes = ["```(?:python|py)([\s\S]*?)```",
-                       "```(?:c)([\s\S]*?)```",
-                       "```(?:golang|go)([\s\S]*?)```",
-                       "```(?:bash|sh)([\s\S]*?)```",
-                       "```(?:zsh)([\s\S]*?)```",
-                       "```(?:rust|rs)([\s\S]*?)```",
-                       "```(?:javascript|js)([\s\S]*?)```"]
-            langs = ["py",
-                     "c",
-                     "go",
-                     "bash",
-                     "zsh",
-                     "rs",
-                     "js"]
-            i = 0
-            while i < len(regexes):
-                r = re.compile(regexes[i])
-                match = r.search(ctx.message.content)
-                if match:
-                    code = match.group(1)
-                    lang = langs[i]
-                    break
+        code = ""
+        lang = ""
+        # TODO read from JSON
+        regexes = ["```(?:python|py)([\s\S]*?)```",
+                    "```(?:c)([\s\S]*?)```",
+                    "```(?:golang|go)([\s\S]*?)```",
+                    "```(?:bash|sh)([\s\S]*?)```",
+                    "```(?:zsh)([\s\S]*?)```",
+                    "```(?:rust|rs)([\s\S]*?)```",
+                    "```(?:javascript|js)([\s\S]*?)```"]
+        langs = ["py",
+                    "c",
+                    "go",
+                    "bash",
+                    "zsh",
+                    "rs",
+                    "js"]
+        i = 0
+        while i < len(regexes):
+            r = re.compile(regexes[i])
+            match = r.search(ctx.message.content)
+            if match:
+                code = match.group(1)
+                lang = langs[i]
+                break
 
-                i += 1
+            i += 1
 
-            if lang == "" or code == "":
-                await ctx.message.add_reaction("❌")
-                await ctx.reply("Unknown language. Please use a formatted code block (e.g. ` ```c`).")
-                return
+        if lang == "" or code == "":
+            await ctx.message.add_reaction("❌")
+            await ctx.reply("Unknown language. Please use a formatted code block (e.g. ` ```c`).")
+            return
 
-            await ctx.message.add_reaction("⏳")
+        await ctx.message.add_reaction("⏳")
 
-            # add default code (#include, etc) if it doesn't have it
-            code = _processing(code=code, lang=lang)
+        # add default code (#include, etc) if it doesn't have it
+        code = _processing(code=code, lang=lang)
 
-            os.makedirs("playground", exist_ok=True)
-            with open("playground/{}.{}".format(ctx.message.id, lang), mode="w") as f:
-                f.write(code)
+        os.makedirs("playground", exist_ok=True)
+        with open("playground/{}.{}".format(ctx.message.id, lang), mode="w") as f:
+            f.write(code)
 
-            try:
-                self.dockerHost.images.build(path="./",
-                                             dockerfile="dockerfiles/{}-Dockerfile".format(lang),
-                                             buildargs={"MESSAGE_ID": str(ctx.message.id)},
-                                             tag="execbot/" + str(ctx.message.id),
-                                             forcerm=True)
-            except docker.errors.BuildError:
-                await ctx.message.remove_reaction("⏳", ctx.me)
-                await ctx.message.add_reaction("❌")
-                await ctx.reply("Your snippet failed to compile. Please double-check syntax and try again.")
-
-                os.remove("playground/{}.{}".format(ctx.message.id, lang))
-                return
-
-            container = self.dockerHost.containers.run("execbot/" + str(ctx.message.id),
-                                                       name=str(ctx.message.id),
-                                                       auto_remove=False,
-                                                       stdout=True,
-                                                       stderr=True,
-                                                       detach=True,
-                                                       cpu_shares=512,
-                                                       mem_limit="512m",
-                                                       device_write_bps=[{"Path": "/dev/sda", "Rate": 500000}],
-                                                       network_disabled=True)
-
-            t = threading.Thread(target=get_logs_from_container,
-                                 name=str(ctx.message.id),
-                                 args=(container, ctx.message.id))
-            t.daemon = True
-            t.start()
+        try:
+            self.dockerHost.images.build(path="./",
+                                            dockerfile="dockerfiles/{}-Dockerfile".format(lang),
+                                            buildargs={"MESSAGE_ID": str(ctx.message.id)},
+                                            tag="execbot/" + str(ctx.message.id),
+                                            forcerm=True)
+        except docker.errors.BuildError:
+            await ctx.message.remove_reaction("⏳", ctx.me)
+            await ctx.message.add_reaction("❌")
+            await ctx.reply("Your snippet failed to compile. Please double-check syntax and try again.")
 
             os.remove("playground/{}.{}".format(ctx.message.id, lang))
+            return
 
-            time_running = 0
-            was_killed = False
+        container = self.dockerHost.containers.run("execbot/" + str(ctx.message.id),
+                                                    name=str(ctx.message.id),
+                                                    auto_remove=False,
+                                                    stdout=True,
+                                                    stderr=True,
+                                                    detach=True,
+                                                    cpu_shares=512,
+                                                    mem_limit="512m",
+                                                    device_write_bps=[{"Path": "/dev/sda", "Rate": 500000}],
+                                                    network_disabled=True)
 
-            while not os.path.exists("playground/{}.log".format(ctx.message.id)):
-                await asyncio.sleep(1)
-                time_running += 1
-                if time_running > 45:
-                    try:
-                        container.kill()
-                    except docker.errors.APIError:
-                        pass
-                    was_killed = True
-                    break
+        t = threading.Thread(target=get_logs_from_container,
+                                name=str(ctx.message.id),
+                                args=(container, ctx.message.id))
+        t.daemon = True
+        t.start()
 
-            if was_killed:
-                await ctx.message.remove_reaction("⏳", ctx.me)
-                await ctx.message.add_reaction("❌")
-                await ctx.reply("Your snippet was terminated because it took too long".format(ctx.author.id))
-            else:
-                await ctx.message.remove_reaction("⏳", ctx.me)
-                await ctx.message.add_reaction("✅")
-                await ctx.reply(file=discord.File("playground/{}.log".format(ctx.message.id)))
-                await self.log(ctx.message.id, lang)
+        os.remove("playground/{}.{}".format(ctx.message.id, lang))
 
-            try:
-                os.remove("playground/{}.log".format(ctx.message.id))
-            except FileNotFoundError:
-                pass
+        time_running = 0
+        was_killed = False
 
-        return
+        while not os.path.exists("playground/{}.log".format(ctx.message.id)):
+            await asyncio.sleep(1)
+            time_running += 1
+            if time_running > 45:
+                try:
+                    container.kill()
+                except docker.errors.APIError:
+                    pass
+                was_killed = True
+                break
+
+        if was_killed:
+            await ctx.message.remove_reaction("⏳", ctx.me)
+            await ctx.message.add_reaction("❌")
+            await ctx.reply("Your snippet was terminated because it took too long".format(ctx.author.id))
+        else:
+            await ctx.message.remove_reaction("⏳", ctx.me)
+            await ctx.message.add_reaction("✅")
+            await ctx.reply(file=discord.File("playground/{}.log".format(ctx.message.id)))
+            await self.log(ctx.message.id, lang)
+
+        try:
+            os.remove("playground/{}.log".format(ctx.message.id))
+        except FileNotFoundError:
+            pass
 
     @_exec.error
     async def _exec_error(self, ctx, error):
