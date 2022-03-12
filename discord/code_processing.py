d6c741c92516de1ec5b8a0950c248eb0a384ee1a
Hunter Hancock <hancock.hunter@gmail.com>
2022-03-12 13:10:04 -0700

    java validated working

--- discord/code_processing.py
+++ discord/code_processing.py
@@ -42,11 +42,11 @@ func main() {{
 import java.util.*;
 import java.util.concurrent.*;
 
-public class Main {
+public class Main {{
         public static void main(String[] args) {{
             {}
         }}
-}"""
+}}"""
     }
 
     regexes = {
